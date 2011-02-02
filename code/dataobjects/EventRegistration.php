2f94fd8b41c6f9a8792422bea05cd20ddd065daa
ajshort <andrewjshort@gmail.com>
2011-02-02 15:35:06 +1100

    Added the event registration data model.

---
+++ code/dataobjects/EventRegistration.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Represents a registration to an event.
+ *
+ * @package silverstripe-eventmanagement
+ */
+class EventRegistration extends DataObject {
+
+	public static $db = array(
+		'Name'  => 'Varchar(255)',
+		'Email' => 'Varchar(255)'
+	);
+
+	public static $has_one = array(
+		'Time'   => 'RegisterableDateTime',
+		'Member' => 'Member'
+	);
+
+	/**
+	 * @return FieldSet
+	 */
+	public function getRegistrationFields() {
+		$fields = new FieldSet(
+			new TextField('Name', _t('EventManager.YOURNAME', 'Your Name')),
+			new EmailField('Email', _t('EventManager.EMAILADDR', 'Email Address'))
+		);
+
+		$this->extend('updateRegistrationFields', $fields);
+		return $fields;
+	}
+
+	/**
+	 * @return Validator
+	 */
+	public function getRegistrationValidator() {
+		$validator = new RequiredFields('Name', 'Email');
+		$this->extend('updateRegistrationValidator', $validator);
+
+		return $validator;
+	}
+
+}
