e64bddfc6b7a09724b544fdcb62e5a2ac3e78a1b
ajshort <andrewjshort@gmail.com>
2011-02-06 08:42:37 +1100

    Added the multiform and itemsetfield dependencies to _config.php.

--- _config.php
+++ _config.php
@@ -6,3 +6,11 @@
 if (!class_exists('Calendar')) {
 	throw new Exception('The Event Management module requires the Event Calendar module.');
 }
+
+if (!class_exists('MultiForm')) {
+	throw new Exception('The Event Management module requires the MultiForm module.');
+}
+
+if (!class_exists('ItemSetField')) {
+	throw new Exception('The Event Management module requires the ItemSetField module.');
+}
