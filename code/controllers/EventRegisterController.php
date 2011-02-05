aba6d5b1a7335c3f063a4ed91e8c2ebe251ca3c5
ajshort <andrewjshort@gmail.com>
2011-02-06 08:34:09 +1100

    Removed the registration logic from EventRegisterController, and updated it to be a wrapper around EventRegisterForm.

--- code/controllers/EventRegisterController.php
+++ code/controllers/EventRegisterController.php
@@ -12,24 +12,23 @@ class EventRegisterController extends Page_Controller {
 	);
 
 	public static $allowed_actions = array(
-		'index',
-		'RegisterForm',
+		'Form',
 		'afterregistration',
 		'confirm'
 	);
 
 	protected $parent;
-	protected $time;
+	protected $datetime;
 
 	/**
 	 * Constructs a new controller for creating a registration.
 	 *
 	 * @param Controller $parent
-	 * @param RegisterableDateTiem $time
+	 * @param RegisterableDateTime $datetime
 	 */
-	public function __construct($parent, $time) {
-		$this->parent = $parent;
-		$this->time   = $time;
+	public function __construct($parent, $datetime) {
+		$this->parent   = $parent;
+		$this->datetime = $datetime;
 
 		parent::__construct();
 	}
@@ -37,161 +36,36 @@ class EventRegisterController extends Page_Controller {
 	public function init() {
 		parent::init();
 
-		if (!$this->time->canRegister()) {
-			return Security::permissionFailure($this, array(
-				'default' => 'You do not have permission to register for this event.'
-			));
-		}
-
-		if ($this->time->Event()->RequireLoggedIn && !Member::currentUserID()) {
+		if ($this->datetime->Event()->RequireLoggedIn && !Member::currentUserID()) {
 			return Security::permissionFailure($this, array(
 				'default' => 'Please log in to register for this event.'
 			));
 		}
 	}
 
+	public function index() {
+		return $this->getViewer('index')->process($this);
+	}
+
 	/**
-	 * @return string
+	 * @return RegisterableDateTime
 	 */
-	public function index($request) {
-		if ($this->time->LimitedPlaces && ($this->time->getRemainingPlaces() < 1)) {
-			$message = _t('EventManagement.NOPLACES', 'This event has no more places available.');
-
-			$controller = $this->customise(array(
-				'Title'   => _t('EventManagement.EVENTISFULL', 'This Event Is Full'),
-				'Content' => "<p>$message</p>"
-			));
-			return $this->getViewer('index')->process($controller);
-		}
-
-		$title = sprintf(
-			_t('EventManagement.REGISTERFOR', 'Register For %s'), $this->time->EventTitle()
-		);
-
-		$form = $this->RegisterForm();
-
-		$name  = $form->dataFieldByName('Name');
-		$email = $form->dataFieldByName('Email');
-
-		if (!$name->Value())  $name->setValue($request->getVar('name'));
-		if (!$email->Value()) $email->setValue($request->getVar('email'));
-
-		$controller = $this->customise(array(
-			'Title' => $title,
-			'Form'  => $form
-		));
-		return $this->getViewer('index')->process($controller);
+	public function getDateTime() {
+		return $this->datetime;
 	}
 
 	/**
 	 * @return Form
 	 */
-	public function RegisterForm() {
-		$fields    = $this->time->getRegistrationFields();
-		$validator = $this->time->getRegistrationValidator();
-
-		$actions = new FieldSet(
-			new FormAction('doRegister', _t('EventMamnager.REGISTER', 'Register'))
-		);
-		return new Form($this, 'RegisterForm', $fields, $actions, $validator);
+	public function Form() {
+		return new EventRegisterForm($this, 'Form');
 	}
 
 	/**
-	 * Handles creating the event registration.
-	 *
-	 * @param array $data
-	 * @param Form  $form
+	 * @return string
 	 */
-	public function doRegister($data, $form) {
-		$registration = new EventRegistration();
-
-		if ($member = Member::currentUser()) {
-			$registration->Name  = $member->getName();
-			$registration->Email = $member->Email;
-		}
-
-		$form->saveInto($registration);
-
-		$registration->EventID  = $this->time->EventID;
-		$registration->TimeID   = $this->time->ID;
-		$registration->MemberID = Member::currentUserID();
-
-		if (!$this->time->Event()->RegEmailConfirm) {
-			$registration->Confirmed = true;
-		}
-
-		try {
-			$registration->write();
-		} catch (ValidationException $e) {
-			Session::set("FormInfo.{$form->FormName()}.data", $form->getData());
-			$form->addErrorMessage(null, $e->getResult()->message(), 'bad');
-			return $this->redirectBack();
-		}
-
-		if ($this->time->Event()->RegEmailConfirm) {
-			$email = new Email();
-
-			$email->setTo($registration->Email);
-			$email->setSubject(sprintf(
-				_t('EventManagement.CONFIRMREGFOR', 'Confirm Registration For %s (%s)'),
-				$this->time->Event()->Title, SiteConfig::current_site_config()->Title));
-
-			$email->setTemplate('EventRegistrationConfirmationEmail');
-			$email->populateTemplate(array(
-				'Registration' => $registration,
-				'Time'         => $this->time,
-				'SiteConfig'   => SiteConfig::current_site_config(),
-				'ConfirmLink'  => Director::absoluteURL(Controller::join_links(
-					$this->Link(), 'confirm', $registration->ID, '?token=' . $registration->Token))
-			));
-
-			$email->send();
-		}
-
-		return $this->redirect($this->Link('afterregistration'));
-	}
-
-	/**
-	 * Shows a thankyou message to the user after they register.
-	 *
-	 * @return array
-	 */
-	public function afterregistration() {
-		return array(
-			'Title'   => $this->time->Event()->AfterRegTitle,
-			'Content' => $this->time->Event()->obj('AfterRegContent')
-		);
-	}
-
-	/**
-	 * Handles the user clicking on the confirm link in a confirmation email.
-	 */
-	public function confirm($request) {
-		$id    = $request->param('ID');
-		$token = $request->getVar('token');
-
-		if (!$rego = DataObject::get_by_id('EventRegistration', $id)) {
-			return $this->httpError(404);
-		}
-
-		if ($rego->Confirmed || $rego->Token != $token) {
-			return $this->httpError(403);
-		}
-
-		try {
-			$rego->Confirmed = true;
-			$rego->write();
-		} catch (ValidationException $e) {
-			return array(
-				'Title'   => _t('EventManagement.COULDNTCONFIRMREG', 'Could Not Confirm Registration'),
-				'Content' => '<p>' . $e->getResult()->message() . '</p>'
-			);
-		}
-
-		return array(
-			'Title'   => $this->time->Event()->AfterConfirmTitle,
-			'Content' => $this->time->Event()->obj('AfterConfirmContent')
-		);
+	public function Title() {
+		return 'Register For ' . $this->datetime->EventTitle();
 	}
 
 	/**
@@ -200,7 +74,7 @@ class EventRegisterController extends Page_Controller {
 	 */
 	public function Link($action = null) {
 		return Controller::join_links(
-			$this->parent->Link(), 'register', $this->time->ID, $action
+			$this->parent->Link(), 'register', $action
 		);
 	}
 
