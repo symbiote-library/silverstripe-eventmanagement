5607ade7757ef05590f1bd05048be9d010c69c89
ajshort <andrewjshort@gmail.com>
2013-02-13 18:31:02 +1100

    Move dependencies to composer.json.

--- _config.php
+++ _config.php
@@ -3,18 +3,6 @@
  * @package silverstripe-eventmanagement
  */
 
-if (!class_exists('Calendar')) {
-	throw new Exception('The Event Management module requires the Event Calendar module.');
-}
-
-if (!class_exists('MultiForm')) {
-	throw new Exception('The Event Management module requires the MultiForm module.');
-}
-
-if (!class_exists('ItemSetField')) {
-	throw new Exception('The Event Management module requires the ItemSetField module.');
-}
-
 // Since the payment module is an option dependency, we need to link
 // registrations to payments via a conditional decorator.
 if (class_exists('Payment')) {
